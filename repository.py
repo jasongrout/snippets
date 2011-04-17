cef24c584fa5181cb5fa4f42311d670722d767c1
Jason Grout <jason.grout@drake.edu>
2011-04-16 23:57:10 -0500

    List the revisions and allow new revision creation.

--- repository.py
+++ repository.py
@@ -2,17 +2,11 @@ class Repository(object):
     def __init__(self, path):
         self.path=path
 
-    def new_revision(self, message, files):
+    def commit(self, user, message, rev, filedata):
         """
         files is a dictionary of the form {'filename': content, 'filename': content}.  Not all of the files in the repository need to be listed.  Any files that are listed are set to the value of content and the changes are committed with the commit message.
         """
-        for filename, content in files:
-            # be careful about not opening filenames that are above self.path in the directory tree!
-            with open(os.path.join(self.path, filename),'w') as f:
-                f.write(content)
-            # need a way of deleting files too
-        self.commit(message)
-
+        pass
     def commit(self, message):
         "Commit the current revision to the repository."
 
@@ -32,7 +26,7 @@ class Repository(object):
         "Return a list of revision ids of ancestors to the specified revision"
         pass
 
-from mercurial import commands, ui, hg, revset
+from mercurial import commands, ui, hg, revset, cmdutil
 
 class HGRepository(Repository):
     def __init__(self, path):
@@ -41,8 +35,27 @@ class HGRepository(Repository):
         self.repo = hg.repository(self.ui, self.path)
         
         
-    def commit(self, message):
-        commands.commit(self.ui, self.repo, message=message, addremove=True)
+    def commit(self, user, message, rev, filedata):
+        """Commit a change.
+
+        Copied from mecurial.commands
+        """
+        from mercurial.context import memctx,memfilectx
+
+        ctx = memctx(
+            repo=self.repo,
+            parents=(rev, None),
+            text = message,
+            files = filedata.keys(),
+            filectxfn = lambda repo, memctx, path: memfilectx(path, filedata[path], False, False, None),
+            user = user,
+        )
+
+        node = self.repo.commitctx(ctx)
+        return str(self.repo[node])
+
+    def message(self, revision):
+        return self.repo[revision].description()
 
     def file(self, filename, revision='tip'):
         "Return the contents of filename at the revision specified."
@@ -50,7 +63,7 @@ class HGRepository(Repository):
         
     def list_files(self, revision='tip'):
         "Return the list of filenames at the specified revision."
-        from mercurial.cmdutils import matchall
+        from mercurial.cmdutil import matchall
         return list(self.repo[revision].walk(matchall(self.repo)))
 
     def create(self, path):
@@ -61,3 +74,5 @@ class HGRepository(Repository):
         "Return a list of revision ids of ancestors to the specified revision"
         return [str(c) for c in self.repo[revision].ancestors()]
 
+    def descendants(self, revision='tip'):
+        return [str(c) for c in self.repo[revision].descendants()]
--- webserver.py
+++ webserver.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect,url_for
+from repository import HGRepository as Repository
+
 app = Flask(__name__)
 
 
@@ -9,6 +11,25 @@ REPO_DIR='repos'
 def list_of_repos():
     return render_template('repos.html', repos=os.listdir(REPO_DIR))
 
+@app.route("/repo/<repo_path>/<rev>/")
+def repo(repo_path,rev):
+    repo=Repository(os.path.join(REPO_DIR, repo_path))
+    file_data=dict((f,repo.file(f,rev)) for f in repo.list_files(rev))
+    return render_template('repo.html', files=file_data, 
+                           repo_path=repo_path, rev=rev,
+                           message=repo.message(rev),
+                           ancestors=reversed(repo.ancestors(rev)),
+                           descendants=repo.descendants(rev))
+
+@app.route("/repo/<repo_path>/<rev>/new_revision", methods=['POST'])
+def new_revision(repo_path, rev):
+    repo=Repository(os.path.join(REPO_DIR, repo_path))
+    message=request.form['message']
+    filedata=request.form.to_dict()
+    filedata.pop('message')
+    new_rev=repo.commit('DEFAULT USER', message, rev, filedata)
+    print "NEW REVISION: ",new_rev
+    return redirect(url_for('repo', repo_path=repo_path, rev=new_rev))
 
 if __name__ == "__main__":
     app.run(debug=True)
